f20a6673d9cf7a035803b9d57978be4d28c7d106
Kanish Kainth <knshkainth2002@gmail.com>
2026-06-25 21:49:00 +0530

    codebase cleanup!

--- frontend/src/services/jobs.js
+++
@@ -1,9 +0,0 @@
-export {
-  recommendedJobs,
-  searchJobs,
-  getJob,
-  saveJob,
-  getTrackedJobs,
-  updateJobStatus,
-  deleteTrackedJob,
-} from "./jobs.service";
--- frontend/src/utils/auth.js
+++
@@ -1,6 +0,0 @@
-export const getToken = () =>
-    localStorage.getItem("access_token");
-
-export const logout = () => {
-    localStorage.removeItem("access_token");
-};
